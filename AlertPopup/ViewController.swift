558810ea1d040e3ed2ffeec92d3cac1d039e01ec
edge <edge@imac-alpha.local>
2018-11-15 14:25:54 +0900

    Initial Commit

---
+++ AlertPopup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertPopup
+//
+//  Created by edge on 15/11/2018.
+//  Copyright © 2018 edge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
